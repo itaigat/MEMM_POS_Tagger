4379267a9fe83f6a4c86213a81f8857a09de66ad
Dor <dor.coh@gmail.com>
2018-11-29 12:26:08 +0200

    added CompData class, and create_dataset method

--- src/utils/decoder.py
+++ src/utils/decoder.py
@@ -80,3 +80,63 @@ class Comp(Dataset):
                 sentence_lst.append(labeled_word[0])
 
             yield tuples, sentence_lst
+
+
+class CompData(Dataset):
+    def get_comp_text(self):
+        return read_file(self.filename)
+
+    def __iter__(self):
+        """
+        iterates over text files, for each position i in sentence create a history tuple (X)
+        and a label (y)
+        :return: X, y, sentences
+        """
+        txt = self.get_comp_text()
+        # split by sentences
+        sentences = txt.split('\n')
+
+        for sent_id, sentence in enumerate(sentences):
+            words = sentence.split(' ')
+            stripped_sentence = []
+            # X, y
+            tuples, tags = [], []
+            # helper
+            word_tag_tuples = []
+
+            for word in words:
+                word_stripped, tag_stripped = word.split('_')
+                word_tag_tuples.append((word_stripped, tag_stripped))
+                stripped_sentence.append(word_stripped)
+
+            for i, word_tag_tuple in enumerate(word_tag_tuples):
+                word = word_tag_tuple[0]
+                tag = word_tag_tuple[1]
+                tags.append(tag)
+                if i == 0:
+                    tuples.append(('*', '*', sent_id, i))
+                elif i == 1:
+                    tuples.append(('*', word_tag_tuples[i-1][1], sent_id, i))
+                else:
+                    u = word_tag_tuples[i - 2][1]  # pre pre tag
+                    v = word_tag_tuples[i - 1][1]  # pre tag
+                    tuples.append((u, v, sent_id, i))
+
+            yield tuples, tags, stripped_sentence
+
+
+def create_dataset(iterable_sentences):
+    """
+    Gets a CompData object and return data ready for training/inference
+    :param iterable_sentences: CompData type
+    :return: X,y and (sentences,sent_sizes)
+    """
+    X, y, sentences, sent_sizes = [], [], [], []
+    for tuples, tags, sentence in iterable_sentences:
+        for i in range(len(tuples)):
+            X.append(tuples[i])
+            y.append(tags[i])
+        sentences.append(sentence)
+        sent_sizes.append(len(tuples))
+
+    return X, y, (sentences, sent_sizes)

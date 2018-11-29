afec1adb982846237710242d8bd20d2e074cc72d
Dor <dor.coh@gmail.com>
2018-11-30 00:14:24 +0200

    cleared main

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 import os
 from os.path import join, dirname
 
-import numpy as np
+from src.utils.decoder import CompData, create_dataset
+from src.utils.classifier import MaximumEntropyClassifier
 
-from src.utils.common import create_all_features
-from src.utils.decoder import Comp, CompData, create_dataset
-from src.utils.params import Params
+if os.name == 'nt':
+    path = join(dirname(os.getcwd()), 'resources', 'train_dev.wtag')
+else:
+    path = join(os.getcwd(), 'resources', 'train_dev.wtag')
 
-# For Windows
-#comp_dataset = Comp(join(dirname(os.getcwd()), 'resources', 'train_dev.wtag'))
+iterable_sentences = CompData(path)
+X_train, y_train = create_dataset(iterable_sentences)
 
-# For linux
-comp_dataset = Comp(join(os.getcwd(), 'resources', 'train_dev.wtag'))
+# sanity check
+print(X_train.shape)
+print(y_train.shape)
 
-# x_lst = []
-#
-# for tuples, sentence in comp_dataset:
-#     print(sentence)
-#     print(tuples)
-#     x_lst.append(create_all_features(Params.feature_functions, Params.feature_lst_functions, tuples, sentence))
-#
-# x = np.concatenate(tuple(x_lst), axis=0)
-# print(x)
-# print(x.shape)
+# at this point we can fit the algorithm
+clf = MaximumEntropyClassifier()
+clf.fit(X_train, y_train)
 
-
-iterable_sentences = CompData(join(os.getcwd(), 'resources', 'train_dev.wtag'))
-X, y, sentences_tuple = create_dataset(iterable_sentences)
-
-print("Sentences:" + str(sentences_tuple[0]))
-print("Sizes: " + str(sentences_tuple[1]))
 
 

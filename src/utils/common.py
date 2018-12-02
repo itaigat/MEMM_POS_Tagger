19efb713061e79eae258c355516f98e3338f081b
Dor <dor.coh@gmail.com>
2018-12-02 18:44:04 +0200

    added pos

--- src/utils/common.py
+++ src/utils/common.py
@@ -12,3 +12,7 @@ def read_file(file_path):
     except Exception as e:
         print(e)
 
+
+poss = ['CC', 'CD', 'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', 'NN', 'NNS', 'NNP', 'NNPS', 'PDT',
+        'POS', 'PRP', 'PRP$', 'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', 'VBG', 'VBN', 'VBP', 'VBZ',
+        'WDT', 'WP', 'WP$', 'WRB', '#', '$', '\'\'', '``', '(', ')', ',', '.', ':']

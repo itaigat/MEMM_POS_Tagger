637bb78d832f6acc78bd642e8bc6d97dd8d92d97
Dor <dor.coh@gmail.com>
2018-12-02 18:44:41 +0200

    cleared main

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 from os.path import join, dirname
 
-from src.utils.decoder import CompData, create_dataset
+from src.utils.decoder import CompData
 from src.utils.classifier import MaximumEntropyClassifier
 
 if os.name == 'nt':
@@ -10,15 +10,5 @@ else:
     path = join(os.getcwd(), 'resources', 'train_dev.wtag')
 
 iterable_sentences = CompData(path)
-X_train, y_train = create_dataset(iterable_sentences)
-
-# sanity check
-print(X_train.shape)
-print(y_train.shape)
-
-# at this point we can fit the algorithm
 clf = MaximumEntropyClassifier()
-clf.fit(X_train, y_train)
-
-
-
+clf.fit(iterable_sentences)

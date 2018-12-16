a3b0e680ffcde337fcd48ed6d62263e9baaff65d
itaigat <itaigat.mail@gmail.com>
2018-12-16 18:33:40 +0200

    main

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ from postagger.utils.decoder import CompData
 
 @timeit
 def main(argv):
-    path = get_data_path('train.wtag')
-    iterable_sentences = CompData(path)
-    clf = MaximumEntropyClassifier(iterable_sentences)
+    train_path = get_data_path('train.wtag')
+    train_sentences = CompData(train_path)
+    clf = MaximumEntropyClassifier(train_sentences)
+    clf.fit(reg=10, verbose=1)
+
+    test_path = get_data_path('test.wtag')
+    t_predict = clf.predict(test_path)
+
     # tests (pass on train_dev and unigram features only): CURRENTLY BREAKS (fix of numeric issues)
     # run_clf_tests(clf, clf.feature_matrix, clf.X, clf.y, clf.sentences)
-    clf.fit(reg=10, verbose=1)
+    # clf.fit(reg=10, verbose=1)
 
 
 if __name__ == '__main__':
